827ff68ce9aadb976d6741e61a2e75bc1af128b5
Anastasiya <sobakapavlova4@gmail.com>
2018-08-23 09:34:07 +0300

    Add EquationTest

---
+++ sandbox/src/test/java/ru.stqa.pft.sandbox/EquationTests.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class EquationTests {
+
+    @Test
+    public void test0(){
+        Equation e = new Equation(1, 1, 1);
+        Assert.assertEquals(e.rootNumber(), 0);
+    }
+    public void test1(){
+        Equation e = new Equation(1, 2, 1);
+        Assert.assertEquals(e.rootNumber(), 1);
+    }
+    public void test2(){
+        Equation e = new Equation(1, 5, 6);
+        Assert.assertEquals(e.rootNumber(), 2);
+    }
+}
