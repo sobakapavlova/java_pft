76d5e56229da2aee0c070683c4214eb42da31dfd
Anastasiya <sobakapavlova4@gmail.com>
2018-11-04 10:04:57 +0300

    3.6 lection done

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -7,7 +7,6 @@ import ru.stqa.pft.addressbook.model.GroupData;
 public class GroupModificationTests extends TestBase {
 
     @Test
-
     public void testGroupModification(){
         app.getNavigationHelper().gotoGroupPage();
         app.getGroupHelper().selectGroup();
