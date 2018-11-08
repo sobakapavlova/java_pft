1c4dcff9d1961a92d8031b42a4f91462d9a21a6f
Anastasiya <sobakapavlova4@gmail.com>
2018-11-08 23:47:54 +0300

    lecture 3.9 done. Lazy implementations

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -6,19 +6,28 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class NavigationHelper extends HelperBase {
 
-    public NavigationHelper(WebDriver wd) {
-        super(wd);    }
+    NavigationHelper(WebDriver wd) {
+        super(wd);
+    }
 
     public void gotoGroupPage() {
-        click(By.linkText("groups"));
+        if (isElementPresent(By.tagName("h1")) &&
+                wd.findElement(By.tagName("h1")).getText().equals("Groups") &&
+                isElementPresent(By.name("new"))) {
+            return;
+        }
+            click(By.linkText("groups"));
     }
 
     public void gotoAddNewPage() {
         click(By.linkText("add new"));
-
     }
 
     public void gotoHomePage() {
+        if (isElementPresent(By.id("maintable"))) {
+            return;
+        }
         click(By.linkText("home"));
     }
 }
+
