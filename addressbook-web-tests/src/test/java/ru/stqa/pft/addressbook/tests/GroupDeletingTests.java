f291c8d2e191bb2756f593e76d79d6c2f16c427a
Anastasiya <sobakapavlova4@gmail.com>
2018-11-13 11:58:44 +0300

    Lecture 4.4 done. Pick up element by index

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletingTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletingTests.java
@@ -15,7 +15,7 @@ public class GroupDeletingTests extends TestBase{
         if(! app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         }
-            app.getGroupHelper().selectGroup();
+            app.getGroupHelper().selectGroup(before -1);
         app.getGroupHelper().DeleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
         int after = app.getGroupHelper().getGroupCount();
