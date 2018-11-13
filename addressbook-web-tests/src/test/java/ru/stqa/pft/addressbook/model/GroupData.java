e42b3a5469d22a5407c16dc4721ab1d69a052f5c
Anastasiya <sobakapavlova4@gmail.com>
2018-11-13 13:31:27 +0300

    Lecture 4.6 done. Code generation for comparing group-lists

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -2,11 +2,33 @@ package ru.stqa.pft.addressbook.model;
 
 import com.sun.xml.internal.bind.v2.TODO;
 
+import java.util.Objects;
+
 public class GroupData {
     private final String name;
     private final String header;
     private final String footer;
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GroupData groupData = (GroupData) o;
+        return Objects.equals(name, groupData.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 
     public GroupData(String name, String header, String footer) {
         this.name = name;
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletingTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletingTests.java
@@ -22,8 +22,10 @@ public class GroupDeletingTests extends TestBase {
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(before.size() - 1, after.size());
-
+        before.remove(before.size() - 1);
+        Assert.assertEquals(before, after);
     }
+
 
 }
 
