cb10f4d5a12555bbd26ce60833efdbeca417bb49
Anastasiya <sobakapavlova4@gmail.com>
2018-11-13 14:40:50 +0300

    Lecture 4.7 done. HashSets comparing of group-lists

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -8,30 +8,46 @@ public class GroupData {
     private final String name;
     private final String header;
     private final String footer;
+    private final String id;
 
+
+    @Override
+    public String toString() {
+        return "GroupData{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                '}';
+    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         GroupData groupData = (GroupData) o;
-        return Objects.equals(name, groupData.name);
+        return Objects.equals(name, groupData.name) &&
+                Objects.equals(id, groupData.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name);
+        return Objects.hash(name, id);
     }
 
-    @Override
-    public String toString() {
-        return "GroupData{" +
-                "name='" + name + '\'' +
-                '}';
+    public String getId() {
+        return id;
     }
+
 
     public GroupData(String name, String header, String footer) {
         this.name = name;
+        this.id = null;
+        this.header = header;
+        this.footer = footer;
+    }
+
+    public GroupData(String name, String id, String header, String footer) {
+        this.name = name;
+        this.id = id;
         this.header = header;
         this.footer = footer;
     }
